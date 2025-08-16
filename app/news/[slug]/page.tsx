5d38f1940aa60773546b5b0de3c0e91383326db9
任意のユーザー名 <tomo.2015cj@gmail.com>
2025-08-15 19:02:08 -0700

    サイト全体にrevalidateを設定する

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,8 +4,6 @@ import Article from "@/app/_components/Articles";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-export const revalidate = 60;
-
 type Props = {
   params: {
     slug: string;
